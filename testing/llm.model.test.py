390151febeb7f6f1ceba0c083efffe3429428327
MoritzValentin <79050824+MoritzValentin@users.noreply.github.com>
2025-11-25 16:29:05 +0100

    populating repo

---
+++ testing/llm.model.test.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b")
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt")
+
+outputs = model.generate(**input_ids)
+
+with open('./output.txt', 'w') as file:
+    file.write(tokenizer.decode(outputs[0]))
+
